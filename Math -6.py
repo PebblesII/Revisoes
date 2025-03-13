ee70124c3a8bd63eef77debcd3e8361e08de677d
PebblesII <shobell70@gmail.com>
2025-03-13 10:26:17 -0300

    Changes

--- Math -6.py
+++ Math -6.py
@@ -5,3 +5,18 @@ def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 clear()
+
+def num_pares():
+    pares = []
+    contador = 0
+
+
+    print("-" * 50)
+    quantidade = int(input("Digite uma quantidade"))
+    print("-" * 50)
+
+    
+    while contador <= quantidade:
+        try:
+            aleatorio = random.randint(1, 100)
+            
